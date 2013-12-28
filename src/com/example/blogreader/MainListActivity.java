29e9b56a8184d2ae6f3e13b632af7d9a5b706ac9
Pete Doherty <ethagnawl@gmail.com>
2013-12-28 16:49:57 -0500

    completed stage 2

---
+++ src/com/example/blogreader/MainListActivity.java
@@ -0,0 +1,87 @@
+package com.example.blogreader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.widget.Toast;
+
+public class MainListActivity extends ListActivity {
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
+	protected String[] mBlogPostTitles;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_list);
+
+		if (isNetworkAvailable()) {
+			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+			getBlogPostsTask.execute();			
+		} else {
+			Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
+		}
+		
+		//Toast.makeText(this, getString(R.string.no_items), Toast.LENGTH_LONG).show();
+	}
+
+	private boolean isNetworkAvailable() {
+		ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        
+        if (networkInfo != null && networkInfo.isConnected()) {
+        	isAvailable = true;
+        }
+        
+        return isAvailable;
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_list, menu);
+		return true;
+	}
+	
+	private class GetBlogPostsTask extends AsyncTask<Object, Void, String> {
+
+		@Override
+		protected String doInBackground(Object... params) {
+			int responseCode = -1;
+			try {
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);	
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+				responseCode = connection.getResponseCode();
+				
+				if (responseCode == HttpURLConnection.HTTP_OK) {
+					InputStream inputStream = connection.getInputStream();
+					Reader reader = new InputStreamReader(inputStream);					
+				} else {
+					Log.i(TAG, "Unsucessful HTTP Response Code: " +  responseCode);
+				}
+			} catch (MalformedURLException e) {
+				Log.e(TAG, "MalformedURLException caught: ", e);
+			} catch (IOException e) {
+				Log.e(TAG, "IOException caught: ", e);
+			} catch (Exception e) {
+				Log.e(TAG, "Exception caught: ", e);
+			}
+			return "Code: " + responseCode;
+		}				
+	}
+}
